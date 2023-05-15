39232bd6a7d3f2d76e3d328c19eddbe3f67c69c8
preetam rana <preetamrana1995@gmail.com>
2023-05-15 18:08:41 +0530

    Add project files.

---
+++ MySQLDatabaseConnectProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using MySqlConnector;
+
+namespace MySQLDatabaseConnectProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MySqlConnection conn = new MySqlConnection("server=localhost;user=root;database=mydata;password=mypassword");
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM emp", conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0] + "| " + reader[1]);
+            }
+            conn.Close();
+        }
+    }
+}
